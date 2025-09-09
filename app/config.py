13698657bd5446f2d5fbc61f7491929bebfe3ce0
moen <moenr49@gmail.com>
2025-09-09 14:28:09 +0300

    debuging: using correct syntax

--- app/config.py
+++ app/config.py
@@ -6,4 +6,3 @@ load_dotenv()
 class Config :
     SQLALCHEMY_DATABASE_URI= os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS= False
-    SECRET_KEY= 'secret'
--- app/routes/student.py
+++ app/routes/student.py
@@ -3,14 +3,18 @@ from flask import Blueprint,jsonify
 # create student blueprint
 
 student_bp=Blueprint("student",__name__)
+@student_bp.route("/",methods=["GET"])
+def home():
+    return "welcome to our API"
 
 # routes and controller
-@student_bp.routes("student/add",method=["POST"])
+@student_bp.route("/student/add",methods=["POST"])
 def add_user ():
     print ("Add user was hit")
     return "Adding user"
 
-@student_bp.routes("/students",method=["GET"])
+@student_bp.route("/students",methods=["GET"])
 def list_users():
     print ("List Student ")
-    return "Listing all students"
+    users= [{"name":"Roy"}]
+    return (jsonify (users), "Listing all students")
