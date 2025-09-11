8a6d34c38534a33a35be3e50b9fb40a622ca921c
moen <moenr49@gmail.com>
2025-09-11 10:56:31 +0300

    first migration init

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,18 +1,25 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint,jsonify,request
 
 # create student blueprint
 
 student_bp=Blueprint("student",__name__,url_prefix="/student")
 
-@student_bp.route("/",methods=["GET","POST"])
+@student_bp.route("/",methods=["GET","POST,PUT"])
 def home():
     return "welcome to our API"
 
 # routes and controller
-@student_bp.route("/add",methods=["POST"])
-def add_student ():
-    print ("Add user was hit")
-    return "Adding user"
+@student_bp.route("/add/json",methods=["POST"])
+def add_student_json ():
+    data=request.get_json()
+    print ("Add user using a json was hit")
+    print(data)
+    return "Adding user via json"
+
+@student_bp.route("/add/form",methods=["POST"])
+def add_student_form ():
+    print ("Add user using a form was hit")
+    return "Adding user via form"
 
 @student_bp.route("/edit",methods=["PUT"])
 def eddit_user ():
