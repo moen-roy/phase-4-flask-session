36df27bf29bfcda6460edd120e082276793b2aff
moen <moenr49@gmail.com>
2025-09-11 07:36:58 +0300

    adding a url prefix

--- app/routes/student.py
+++ app/routes/student.py
@@ -2,19 +2,25 @@ from flask import Blueprint,jsonify
 
 # create student blueprint
 
-student_bp=Blueprint("student",__name__)
+student_bp=Blueprint("student",__name__,url_prefix="/student")
+
 @student_bp.route("/",methods=["GET"])
 def home():
     return "welcome to our API"
 
 # routes and controller
-@student_bp.route("/student/add",methods=["POST"])
-def add_user ():
+@student_bp.route("/add",methods=["POST"])
+def add_student ():
     print ("Add user was hit")
     return "Adding user"
 
-@student_bp.route("/students",methods=["GET"])
+@student_bp.route("/edit",methods=["PUT"])
+def eddit_user ():
+    print ("Edditing user was hit")
+    return "Edditing user"
+
+@student_bp.route("/list",methods=["GET"])
 def list_users():
     print ("List Student ")
     users= [{"name":"Roy"}]
-    return (jsonify (users), "Listing all students")
+    return ( "Listing all students")
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from app import create_app
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
-app= create_app()
 
-if __name__== '__main__':
+app = create_app()
+
+if __name__ == '__main__':
     app.run(debug=True)
