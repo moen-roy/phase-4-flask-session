870e45cb9e8f6346448495b62a15e81312c2b14e
moen <moenr49@gmail.com>
2025-09-14 21:30:41 +0300

    adding data in the data base indirectly

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,4 +1,7 @@
 from flask import Blueprint,jsonify,request
+from app.models import Student
+from app.db import db
+import re  
 
 # create student blueprint
 
@@ -11,15 +14,47 @@ def home():
 # routes and controller
 @student_bp.route("/add/json",methods=["POST"])
 def add_student_json ():
-    data=request.get_json()
+
     print ("Add user using a json was hit")
+    data=request.get_json()
+    # checking name
+    name= data.get("name")
+    email=data.get("email")
+
+    if not name:
+        return jsonify({"error":"Name is invalid"}),400
+    if not email:
+        return jsonify({"error":"Email invalid"}),400
+    email_regex= r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    if not re.match(email_regex,email):
+        return jsonify({"error":"Incorect email format"})
+
+    exists= Student.query.filter_by(email=email).first()
+    if exists:
+        return jsonify({"error":"Email in use"}),400
+    
+    new_student=Student(name=name,email=email)
+    db.session.add(new_student)
+    db.session.commit()
+    return jsonify({
+        "message":"Student added",
+        "name":new_student.name,
+        "email":new_student.email,
+        "created_at":new_student.created_at
+
+    })
+
+
+
+    
+
     print(data)
-    return "Adding user via json"
+    return "Adding user via json",200
 
 @student_bp.route("/add/form",methods=["POST"])
 def add_student_form ():
     print ("Add user using a form was hit")
-    return "Adding user via form"
+    return "Adding user via form",200
 
 @student_bp.route("/edit",methods=["PUT"])
 def eddit_user ():
